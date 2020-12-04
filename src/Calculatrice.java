77059d881c43cc717246cbdef19cb2b12adca353
Eugène VALTY <eugenevalty@gmail.com>
2020-12-04 22:22:59 +0100

    extension 3 '''implementée'''

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -5,7 +5,10 @@ import java.util.stream.Collectors;
 public final class Calculatrice {
 	private final Scanner useVal = new Scanner(System.in);
 	private String strVal = "";
+	// Les objets de stack et histo devrait implementer clonnable...
 	private Stack<Object> stack = new Stack<> ();
+	private Stack<Object> histo = new Stack<> ();
+	private Map <String, Object> variables = new HashMap<>();
 	private Map<String,Map <Signature, Operation>> dico = new HashMap<>();
 
 
@@ -24,23 +27,31 @@ public final class Calculatrice {
 			Signature sig = s.getKey();
 			if(sig.nbArgs > stack.size()) continue;
 			boolean valid = true;
-			for(int i = 0; i < sig.nbArgs; i++) if(stack.get(stack.size() - 1 - i).getClass() != sig.getTypeArgs().get(i)){ valid = false; break; }
+			for(int i = 0; i < sig.nbArgs; i++)
+				if(!sig.getTypeArgs().get(i).isInstance(stack.get(stack.size() - 1 - i))){ valid = false; break; }
 			if(valid) return s;
 		}
 		return null;
 	}
 
 	private void addOperations(){
-		String[] operations = new String[]{"+", "-","*","/","AND","OR","NOT"};
+		String[] operations = new String[]{"+", "-","*","/","AND","OR","NOT","^"};
 		for(String s : operations) dico.put(s, new HashMap<>());
-		dico.get("+").put(new Signature(2,List.of(Integer.class,Integer.class)), args -> (Integer) args[0] + (Integer) args[1]);
-		dico.get("-").put(new Signature(2,List.of(Integer.class,Integer.class)), args -> (Integer) args[0] - (Integer) args[1]);
-		dico.get("*").put(new Signature(2,List.of(Integer.class,Integer.class)), args -> (Integer) args[0] * (Integer) args[1]);
-		dico.get("/").put(new Signature(2,List.of(Integer.class,Integer.class)), args -> (Integer) args[0] / (Integer) args[1]);
+		dico.get("+").put(new Signature(2,List.of(Integer.class,Integer.class)),
+				args -> (Integer) args[0] + (Integer) args[1]);
+		dico.get("-").put(new Signature(2,List.of(Integer.class,Integer.class)),
+				args -> (Integer) args[0] - (Integer) args[1]);
+		dico.get("*").put(new Signature(2,List.of(Integer.class,Integer.class)),
+				args -> (Integer) args[0] * (Integer) args[1]);
+		dico.get("/").put(new Signature(2,List.of(Integer.class,Integer.class)),
+				args -> (Integer) args[0] / (Integer) args[1]);
 
-		dico.get("AND").put(new Signature(2,List.of(Boolean.class,Boolean.class)), args -> (Boolean) args[0] && (Boolean) args[1]);
-		dico.get("OR").put(new Signature(2,List.of(Boolean.class,Boolean.class)), args -> (Boolean) args[0] || (Boolean) args[1]);
-		dico.get("NOT").put(new Signature(1,List.of(Boolean.class)), args -> !(Boolean) args[0]);
+		dico.get("AND").put(new Signature(2,List.of(Boolean.class,Boolean.class)),
+				args -> (Boolean) args[0] && (Boolean) args[1]);
+		dico.get("OR").put(new Signature(2,List.of(Boolean.class,Boolean.class)),
+				args -> (Boolean) args[0] || (Boolean) args[1]);
+		dico.get("NOT").put(new Signature(1,List.of(Boolean.class)),
+				args -> !(Boolean) args[0]);
 	}
 
 	private Object[] toArrayInRange(int n){
@@ -63,14 +74,42 @@ public final class Calculatrice {
 			mots.addAll(Arrays.asList(strVal.trim().split("[ \t]+")));
 
 			for (String s : mots) {
+				s = s.trim();
 				Map m = dico.get(s.toUpperCase());
 				if(m != null){
 					Map.Entry<Signature,Operation> o = getCorrespondingOperation(m);
 					Object obj = null;
 					if(o != null) obj = o.getValue().compute(toArrayInRange(o.getKey().nbArgs));
-					if(obj != null) { stack.push(obj); continue; }
+					if(obj != null) {
+						stack.push(obj);
+						histo.push(stack.peek());
+						continue; }
 				}
-				stack.push(TypeParser.parse(s));
+				try{
+					Object obj;
+					if(s.contains("!")){
+						String sub = s.substring(1, s.length());
+						variables.put(sub, stack.pop());
+						continue;
+					}
+					if(s.contains("?")){
+						String sub = s.substring(1, s.length());
+						stack.push(variables.get(sub));
+						continue;
+					}else if(s.contains("hist")){
+						int i = Integer.valueOf(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
+						obj = histo.get((i >= 0)? i : histo.size() + i);
+					}else if(s.contains("pile")){
+						int i = Integer.valueOf(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
+						obj = stack.get((i >= 0)? i : stack.size() + i);
+					}else{
+						obj = TypeParser.parse(s);
+					}
+					stack.push(obj);
+					histo.push(stack.peek());
+				}catch(Exception e){
+					System.out.println(e);
+				}
 			}
 		}
 	}
