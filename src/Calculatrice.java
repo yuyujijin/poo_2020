57a6a5990c226a1653cf17d8606d437bcca71c47
Eugène VALTY <eugenevalty@gmail.com>
2020-12-17 20:36:08 +0100

    quelques petites corrections  + ajout de commentaires "

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -25,9 +25,9 @@ public final class Calculatrice {
 	private Map.Entry<Signature,Operation> getCorrespondingOperation(Map<Signature,Operation> m){
 		for(Map.Entry<Signature,Operation> s : m.entrySet()){
 			Signature sig = s.getKey();
-			if(sig.nbArgs > stack.size()) continue;
+			if(sig.getNbArgs() > stack.size()) continue;
 			boolean valid = true;
-			for(int i = 0; i < sig.nbArgs; i++)
+			for(int i = 0; i < sig.getNbArgs(); i++)
 				if(!sig.getTypeArgs().get(i).isInstance(stack.get(stack.size() - 1 - i))){ valid = false; break; }
 			if(valid) return s;
 		}
@@ -37,21 +37,32 @@ public final class Calculatrice {
 	private void addOperations(){
 		String[] operations = new String[]{"+", "-","*","/","AND","OR","NOT","^"};
 		for(String s : operations) dico.put(s, new HashMap<>());
-		dico.get("+").put(new Signature(2,List.of(Integer.class,Integer.class)),
+		// opérations usuelles sur les entiers
+		dico.get("+").put(new Signature(List.of(Integer.class,Integer.class)),
 				args -> (Integer) args[0] + (Integer) args[1]);
-		dico.get("-").put(new Signature(2,List.of(Integer.class,Integer.class)),
+		dico.get("-").put(new Signature(List.of(Integer.class,Integer.class)),
 				args -> (Integer) args[0] - (Integer) args[1]);
-		dico.get("*").put(new Signature(2,List.of(Integer.class,Integer.class)),
+		dico.get("*").put(new Signature(List.of(Integer.class,Integer.class)),
 				args -> (Integer) args[0] * (Integer) args[1]);
-		dico.get("/").put(new Signature(2,List.of(Integer.class,Integer.class)),
+		dico.get("/").put(new Signature(List.of(Integer.class,Integer.class)),
 				args -> (Integer) args[0] / (Integer) args[1]);
 
-		dico.get("AND").put(new Signature(2,List.of(Boolean.class,Boolean.class)),
+		// opérations 'et' 'ou' 'non' sur les booléens
+		dico.get("AND").put(new Signature(List.of(Boolean.class,Boolean.class)),
 				args -> (Boolean) args[0] && (Boolean) args[1]);
-		dico.get("OR").put(new Signature(2,List.of(Boolean.class,Boolean.class)),
+		dico.get("OR").put(new Signature(List.of(Boolean.class,Boolean.class)),
 				args -> (Boolean) args[0] || (Boolean) args[1]);
-		dico.get("NOT").put(new Signature(1,List.of(Boolean.class)),
+		dico.get("NOT").put(new Signature(List.of(Boolean.class)),
 				args -> !(Boolean) args[0]);
+
+		// opérations usuelles sur les fractions
+		dico.get("+").put(new Signature(List.of(Fraction.class,Fraction.class)),
+				args -> Fraction.sum((Fraction) args[0],(Fraction) args[1]));
+		// addition entre des entiers et des fractions
+		dico.get("+").put(new Signature(List.of(Fraction.class,Integer.class)),
+				args -> Fraction.sum((Fraction) args[0],new Fraction((Integer) args[1],1)));
+		dico.get("+").put(new Signature(List.of(Integer.class,Fraction.class)),
+				args -> Fraction.sum(new Fraction((Integer) args[0],1),(Fraction) args[1]));
 	}
 
 	private Object[] toArrayInRange(int n){
@@ -73,38 +84,50 @@ public final class Calculatrice {
 			Stack<String> mots = new Stack<>();
 			mots.addAll(Arrays.asList(strVal.trim().split("[ \t]+")));
 
+			//pour chaque mot dans la ligne que l'on vient de rentrer
 			for (String s : mots) {
 				s = s.trim();
+				//on récupère l'opération correspondante (si elle existe)
 				Map m = dico.get(s.toUpperCase());
 				if(m != null){
 					Map.Entry<Signature,Operation> o = getCorrespondingOperation(m);
 					Object obj = null;
-					if(o != null) obj = o.getValue().compute(toArrayInRange(o.getKey().nbArgs));
+					// on compute si possible
+					if(o != null) obj = o.getValue().compute(toArrayInRange(o.getKey().getNbArgs()));
 					if(obj != null) {
+						// et si la computation a été faite, on push le resultat
 						stack.push(obj);
 						histo.push(stack.peek());
 						continue; }
 				}
+				// dans le cas ou on le mot ne correspond a aucune opération connue
 				try{
-					Object obj;
-					if(s.contains("!")){
+					// si le mot commence par '!', on stock une variable avec en nom le reste du mot (sans le '!')
+					if(s.charAt(0) == '!'){
 						String sub = s.substring(1, s.length());
 						variables.put(sub, stack.pop());
 						continue;
 					}
-					if(s.contains("?")){
+					// si le mot commence par '?', on empile le valeur de la variable stockée
+					if(s.charAt(0) == '?'){
 						String sub = s.substring(1, s.length());
 						stack.push(variables.get(sub));
 						continue;
-					}else if(s.contains("hist")){
+					}
+					Object obj;
+					// si on execute la commande 'hist', obj prend la valeur de l'historique demandée
+					if(s.length() > 4 && s.substring(0,4).equals("hist")){
 						int i = Integer.valueOf(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
 						obj = histo.get((i >= 0)? i : histo.size() + i);
-					}else if(s.contains("pile")){
+					// 'pile', obj prend la valeur de la pile demandée
+					}else if(s.length() > 4 && s.contains("pile")){
 						int i = Integer.valueOf(s.substring(s.indexOf("(") + 1, s.indexOf(")")));
 						obj = stack.get((i >= 0)? i : stack.size() + i);
+					// sinon, on tente de parser le mot lu
 					}else{
 						obj = TypeParser.parse(s);
 					}
+					// enfin, si on a pu récuperer un objet, on le push dans la pile et dans l'historique (clone...?)
 					stack.push(obj);
 					histo.push(stack.peek());
 				}catch(Exception e){
@@ -119,14 +142,13 @@ public final class Calculatrice {
 	}
 
 	private final static class Signature{
-		private int nbArgs;
 		private List<Class> typeArgs;
 
-		public Signature(int n, List<Class> t){
-			nbArgs = n; typeArgs = new ArrayList<>(t);
+		public Signature(List<Class> t){
+			typeArgs = new ArrayList<>(t);
 		}
 
-		public int getNbArgs(){ return nbArgs; }
+		public int getNbArgs(){ return typeArgs.size(); }
 		public List<Class> getTypeArgs(){ return new ArrayList<>(typeArgs); }
 	}
 
@@ -137,6 +159,7 @@ public final class Calculatrice {
 			Object o;
 			if((o = parseInt(s)) != null) return o;
 			if((o = parseBool(s)) != null) return o;
+			if((o = parseFrac(s)) != null) return o;
 			throw new IllegalArgumentException("Type de l'argument '"+s+"' inconnue.");
 		}
 
@@ -151,6 +174,18 @@ public final class Calculatrice {
 		private static Boolean parseBool(String s){
 			if(s.toLowerCase().equals("true")) return true;
 			if(s.toLowerCase().equals("false")) return false;
+			return null;
+		}
+
+		private static Fraction parseFrac(String s){
+			if(s.contains("/"))
+				try{
+					int d, n;
+					n = Integer.parseInt(s.substring(0,s.indexOf("/")));
+					d = Integer.parseInt(s.substring(s.indexOf("/") + 1));
+					return new Fraction(n,d);
+				}catch(Exception e){
+				}
 			return null;
 		}
 	}
--- src/Fraction.java
+++ src/Fraction.java
@@ -4,20 +4,21 @@ public final class Fraction{
         if(d == 0) throw new IllegalArgumentException("Fraction avec un dénominateur a 0.");
         numérateur = n; dénominateur = d;
     }
-    public static Fraction sum(Fraction f1, Fraction f2){
+    public static Object sum(Fraction f1, Fraction f2){
         return new Fraction(f1.numérateur * f2.dénominateur + f2.numérateur * f1.dénominateur, f1.dénominateur * f2.dénominateur).reduce();
     }
-    public static Fraction substract(Fraction f1, Fraction f2){
+    public static Object substract(Fraction f1, Fraction f2){
         return new Fraction(f1.numérateur * f2.dénominateur - f2.numérateur * f1.dénominateur, f1.dénominateur * f2.dénominateur).reduce();
     }
-    public static Fraction multiplicate(Fraction f1, Fraction f2){
+    public static Object multiplicate(Fraction f1, Fraction f2){
         return new Fraction(f1.numérateur * f2.numérateur, f1.dénominateur * f2.dénominateur).reduce();
     }
-    public static Fraction divide(Fraction f1, Fraction f2){
+    public static Object divide(Fraction f1, Fraction f2){
         return new Fraction(f1.numérateur * f2.dénominateur, f1.dénominateur * f2.numérateur).reduce();
     }
-    private Fraction reduce(){
+    private Object reduce(){
         int pgcd = PGCD(numérateur,dénominateur);
+        if(dénominateur/pgcd == 1) return numérateur/pgcd;
         return new Fraction(numérateur/pgcd,dénominateur/pgcd);
     }
     private int PGCD(int n1, int n2) {
@@ -26,6 +27,7 @@ public final class Fraction{
         }
         return PGCD(n2, n1 % n2);
     }
+
     public String toString(){
         return numérateur+"/"+dénominateur;
     }
