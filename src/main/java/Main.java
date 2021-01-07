4a3915531c3ec210d409a7b696b8c89b6cd4307e
Eugene VALTY <eugenevalty@gmail.com>
2021-01-07 12:44:58 +0100

    rajout de 'run'

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Application.launch(CalculatorView.class,args);
+    }
+}
